6afc02e34afee109a7f5c3cb64931f40099d4250
Salah Saleh <salahsaleh1015@gmail.com>
2024-08-20 16:27:56 +0300

    String Data Types

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,24 @@
 
 void main(){
 
-  // Data Types
+String text = 'my name is salah saleh';
+print(text);
 
- // 1-  primitive data type
- // {String , int , double , bool , dynamic}
+text = 'salah\'s fav hoppy is programming';
+print(text);
 
- // 2- collection data type
+text = 'salah\t s fav hoppy is programming';
+print(text);
 
+text = 'salah\n s fav hoppy is programming';
+print(text);
 
+text = r'salah\ns fav hoppy is programming';
+print(text);
+
+// \t = tab
+// \n = new line
+// r"" = row string
 
 }
 
