e4326f82066606abfb03aef09b5aaeffb906da4b
Salah Saleh <salahsaleh1015@gmail.com>
2024-08-20 16:33:56 +0300

    int Data Types

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,8 @@
 
 
 void main(){
-
-String text = 'my name is salah saleh';
-print(text);
-
-text = 'salah\'s fav hoppy is programming';
-print(text);
-
-text = 'salah\t s fav hoppy is programming';
-print(text);
-
-text = 'salah\n s fav hoppy is programming';
-print(text);
-
-text = r'salah\ns fav hoppy is programming';
-print(text);
-
-// \t = tab
-// \n = new line
-// r"" = row string
+int age = 24;
+print("My age is $age");
 
 }
 
