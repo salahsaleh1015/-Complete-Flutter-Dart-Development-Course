4843634f4e8ba8af2bd4efcc06aeaf10649857b7
Salah Saleh <salahsaleh1015@gmail.com>
2024-08-24 19:24:36 +0300

    if Else

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,11 @@
-
-
-
 void main() {
+  // if Else
 
-   // logical operators
+  bool isWeatherGood = false;
 
-   int mathScore = 60;
-   int englishScore = 90;
-   int physicsScore = 100;
-
-   bool isPassed = mathScore >= 60 && englishScore >= 60 && physicsScore >= 60;
-
-
-   if(isPassed) {
-
-     print("CONGRATS YOU PASSED");
-
-   }
-
-   // creating website [Admin - moderator]
-
-  bool isAdmin = false;
-  bool isModerator = true;
-
-  if(isAdmin || isModerator) {
-    print("you have access to this site");
+  if (isWeatherGood) {
+    print("Go to the Sea");
+  } else {
+    print("Go to the cinema");
   }
-
 }
-  
-  
-  
