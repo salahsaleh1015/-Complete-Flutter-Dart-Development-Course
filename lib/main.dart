cf0e6ead7ebfb2695a0c5503f4dc1f4513f126a6
Salah Saleh <salahsaleh1015@gmail.com>
2024-08-24 19:17:12 +0300

    logical operators

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@
 
 void main() {
 
-   // conditional statement
+   // logical operators
 
    int mathScore = 60;
-   bool isPassed = mathScore >= 60;
+   int englishScore = 90;
+   int physicsScore = 100;
+
+   bool isPassed = mathScore >= 60 && englishScore >= 60 && physicsScore >= 60;
 
 
    if(isPassed) {
@@ -14,6 +17,15 @@ void main() {
      print("CONGRATS YOU PASSED");
 
    }
+
+   // creating website [Admin - moderator]
+
+  bool isAdmin = false;
+  bool isModerator = true;
+
+  if(isAdmin || isModerator) {
+    print("you have access to this site");
+  }
 
 }
   
