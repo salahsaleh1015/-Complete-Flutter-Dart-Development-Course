48d59ff7fcd1710168e7b4c8cbc9ac6361936b24
Salah Saleh <salahsaleh1015@gmail.com>
2024-08-24 18:24:06 +0300

    conditional statement 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,16 @@
 
 void main() {
 
-   // comparison operators
+   // conditional statement
 
-   int arabicScore = 30;
-   int englishScore = 40;
-
-   bool isArabicScoreSmaller = arabicScore < englishScore;
-   print(isArabicScoreSmaller);
-   bool isArabicScoreBigger = arabicScore > englishScore;
-   print(isArabicScoreBigger);
-   bool isArabicScoreEqual = arabicScore == englishScore;
-   print(isArabicScoreEqual);
-   bool isArabicScoreNotEqual = arabicScore != englishScore;
-   print(isArabicScoreNotEqual);
-   bool isArabicScoreSmallerOrEqual = arabicScore <= englishScore;
-   print(isArabicScoreSmallerOrEqual);
-   bool isArabicScoreBiggerOrEqual = arabicScore >= englishScore;
-   print(isArabicScoreBiggerOrEqual);
+   bool isWeatherGood = true;
 
 
+   if(isWeatherGood){
+
+     print("GO TO HE SEA");
+
+   }
 
 }
   
