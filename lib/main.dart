157bccdccbdb7fcc47838d42ddbb3a1f35016c68
Salah Saleh <salahsaleh1015@gmail.com>
2024-08-26 16:39:14 +0300

    named parameters

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 void main() {
-  //  optional parameters
+  //  named parameters
 
-  print(calculateFinalPrice(200, 10));
+  print(calculateFinalPrice(price: 150, discount: 10));
 
-  print(calculateFinalPrice(150));
+  print(calculateFinalPrice(price: 150));
 }
 
-double calculateFinalPrice(double price, [double discount = 0]) {
+double calculateFinalPrice({required double price, double discount = 0}) {
   double finalPrice = price - (price * discount / 100);
 
   return finalPrice;
