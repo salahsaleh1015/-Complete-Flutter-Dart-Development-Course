c7837203ffad5aa788dc0ec6511fb4087b1933b5
Salah Saleh <salahsaleh1015@gmail.com>
2024-09-03 17:26:53 +0300

    inheritance

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,51 @@
 import 'package:complete_flutter_dart_development_course/human.dart';
 
 void main() {
-  Human salah = Human(age: 33, height: 180, skinColor: "black", weight: 95);
+  Lion lion = Lion();
+  Dog dog = Dog();
+  Cat cat = Cat();
 
-  salah.numOfArms = 8;
 
-  print(salah.getNumOfArms);
+
+  cat.meow();
+  cat.move();
+  dog.bark();
+  dog.eat();
+  lion.roar();
+  lion.sleep();
+
+
+}
+
+class Animal {
+  int numOfLimps = 4;
+  void eat() {
+    print('eat');
+  }
+
+  void sleep() {
+    print('sleep');
+  }
+
+  void move() {
+    print('move');
+  }
+}
+
+class Lion extends Animal{
+  void roar() {
+    print('roar');
+  }
+}
+
+class Dog extends Animal{
+  void bark() {
+    print('bark');
+  }
+}
+
+class Cat extends Animal{
+  void meow() {
+    print('meow');
+  }
 }
