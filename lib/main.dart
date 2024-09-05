c6b467a2239a5b26836e41ba0c5ad6eee844b041
Salah Saleh <salahsaleh1015@gmail.com>
2024-09-05 18:29:28 +0300

    material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-import 'dart:io';
 
-void main() {
-  List<String> names = ['salah'];
 
-  try {
-    print(names[5]);
-  } on RangeError {
-    print('sorry there is no name with index 5');
-  } on HttpException {
-    print('sorry there is no name with index 5');
-  } catch (e) {
-    print('sorry there is no name with index 5');
-  }
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    )
+  );
 }
