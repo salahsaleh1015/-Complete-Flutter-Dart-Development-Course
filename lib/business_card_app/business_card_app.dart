8061be4a2657cb2f279395c1c7564ca4d00ebec9
Salah Saleh <salahsaleh1015@gmail.com>
2024-09-09 21:08:23 +0300

    Business card app (margin)

--- lib/business_card_app/business_card_app.dart
+++ lib/business_card_app/business_card_app.dart
@@ -36,62 +36,58 @@ class BusinessCardApp extends StatelessWidget {
             endIndent: 50,
             thickness: 2,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              height: 65,
-              child: const Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Icon(
-                      Icons.phone,
-                      color: Color(0xFF314f6a),
-                      size: 32,
-                    ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            height: 65,
+            child: const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Icon(
+                    Icons.phone,
+                    color: Color(0xFF314f6a),
+                    size: 32,
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 15),
-                    child: Text(
-                      "(+20) 1127504745",
-                      style: TextStyle(fontSize: 24),
-                    ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 15),
+                  child: Text(
+                    "(+20) 1127504745",
+                    style: TextStyle(fontSize: 24),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              height: 65,
-              child: const Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Icon(
-                      Icons.mail,
-                      color: Color(0xFF314f6a),
-                      size: 32,
-                    ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            height: 65,
+            child: const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Icon(
+                    Icons.mail,
+                    color: Color(0xFF314f6a),
+                    size: 32,
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 15),
-                    child: Text(
-                      "salahsaleh1015@gmail.com",
-                      style: TextStyle(fontSize: 24),
-                    ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 15),
+                  child: Text(
+                    "salahsaleh1015@gmail.com",
+                    style: TextStyle(fontSize: 24),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           )
         ],
