d23d771a695bc728c49ff82efeacd4dec6881f6f
Salah Saleh <salahsaleh1015@gmail.com>
2024-09-09 20:18:43 +0300

    Business card app (container and spacer)

--- lib/business_card_app/business_card_app.dart
+++ lib/business_card_app/business_card_app.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class BusinessCardApp extends StatelessWidget {
@@ -6,27 +5,106 @@ class BusinessCardApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      backgroundColor:Color(0xFF314f6a),
-      body: Center(
-        child: Column(
-         children: [
-           CircleAvatar(
+    return Scaffold(
+      backgroundColor: const Color(0xFF314f6a),
+      body: Column(
+        children: [
+          const CircleAvatar(
             radius: 105,
             backgroundColor: Colors.white,
-             child:CircleAvatar(
-               radius: 100,
-               backgroundImage: AssetImage("images/business_image.jpeg"),
-             ) ,
-           ),
-           Text("Salah Saleh", style: TextStyle(
-             color: Colors.white,
-             fontSize: 30,
-             fontFamily: "Pacifico",
-             fontWeight: FontWeight.bold
-           ))
-         ],
-        ),
+            child: CircleAvatar(
+              radius: 100,
+              backgroundImage: AssetImage("images/business_image.jpeg"),
+            ),
+          ),
+          const Text("Salah Saleh",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontFamily: "Pacifico",
+                  fontWeight: FontWeight.bold)),
+          const Text("Flutter Developer",
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 25,
+                  fontWeight: FontWeight.normal)),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 16,
+              right: 16,
+              top: 8,
+              bottom: 8,
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+
+              height: 65,
+              child: const Row(
+                children: [
+
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Icon(
+                                    Icons.phone,
+                      color: Color(0xFF314f6a),
+                      size: 32,
+                    ),
+                  ),
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Text("(+20) 1127504745",style: TextStyle(
+                    fontSize: 24
+                  ),),
+                  Spacer(
+                    flex: 2,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 16,
+              right: 16,
+              top: 8,
+              bottom: 8,
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+
+              height: 65,
+              child: const Row(
+                children: [
+
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Icon(
+                      Icons.mail,
+                      color: Color(0xFF314f6a),
+                      size: 32,
+                    ),
+                  ),
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Text("salahsaleh1015@gmail.com",style: TextStyle(
+                      fontSize: 24
+                  ),),
+                  Spacer(
+                    flex: 2,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
