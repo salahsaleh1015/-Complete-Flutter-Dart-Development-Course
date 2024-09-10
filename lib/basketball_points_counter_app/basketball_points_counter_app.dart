f7645cd6b7c5103ca9e32a2d395ea0f0966f7049
Salah Saleh <salahsaleh1015@gmail.com>
2024-09-10 17:49:28 +0300

    Basketball Points Counter app (buttons widget)

--- lib/basketball_points_counter_app/basketball_points_counter_app.dart
+++ lib/basketball_points_counter_app/basketball_points_counter_app.dart
@@ -16,21 +16,70 @@ class BasketballPointsCounterApp extends StatelessWidget {
           ),
         ),
       ),
-
-
       body: Column(
         children: [
-          Text("Team A",style: TextStyle(
-            fontSize: 40,
-
-          ),),
-          Text("0",style: TextStyle(
-            fontSize: 150,
-
-          ),),
+          const Text(
+            "Team A",
+            style: TextStyle(
+              fontSize: 40,
+            ),
+          ),
+          const Text(
+            "0",
+            style: TextStyle(
+              fontSize: 150,
+            ),
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(80, 50),
+                backgroundColor: Colors.orange,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                "Add 1 Point",
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              )),
+          const Spacer(
+            flex: 1,
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(80, 50),
+                backgroundColor: Colors.orange,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                "Add 2 Point",
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              )),
+          const Spacer(
+            flex: 1,
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(80, 50),
+                backgroundColor: Colors.orange,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                "Add 3 Point",
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              )),
+          const Spacer(
+            flex:12,
+          ),
         ],
       ),
-
     );
   }
 }
