f238754906293c5abdcd1ae8fd537135d1aaca5b
Salah Saleh <salahsaleh1015@gmail.com>
2024-09-17 21:56:51 +0300

    toku app (creating numbers list)

--- lib/toku_app/screens/numbers_page.dart
+++ lib/toku_app/screens/numbers_page.dart
@@ -10,33 +10,86 @@ class NumbersPage extends StatelessWidget {
     enName: "one",
     jpName: "ichi",
   );
+
+  final List<Number> numbers = const [
+    Number(
+      image: 'assets/images/numbers/number_one.png',
+      enName: "one",
+      jpName: "ichi",
+    ),
+    Number(
+        enName: "two",
+        jpName: "ni",
+        image: 'assets/images/numbers/number_two.png'),
+    Number(
+        enName: "three",
+        jpName: "san",
+        image: 'assets/images/numbers/number_three.png'),
+    Number(
+        enName: "four",
+        jpName: "yon",
+        image: 'assets/images/numbers/number_four.png'),
+    Number(
+        enName: "five",
+        jpName: "go",
+        image: 'assets/images/numbers/number_five.png'),
+    Number(
+        enName: "six",
+        jpName: "roku",
+        image: 'assets/images/numbers/number_six.png'),
+    Number(
+        enName: "seven",
+        jpName: "nana",
+        image: 'assets/images/numbers/number_seven.png'),
+    Number(
+        enName: "eight",
+        jpName: "hachi",
+        image: 'assets/images/numbers/number_eight.png'),
+    Number(
+        enName: "nine",
+        jpName: "kyu",
+        image: 'assets/images/numbers/number_nine.png'),
+    Number(
+        enName: "ten",
+        jpName: "juu",
+        image: 'assets/images/numbers/number_ten.png'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(
-              Icons.arrow_back,
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              )),
+          backgroundColor: const Color(0xff533d35),
+          title: const Text(
+            "Numbers",
+            style: TextStyle(
+              fontSize: 24,
               color: Colors.white,
-            )),
-        backgroundColor: const Color(0xff533d35),
-        title: const Text(
-          "Numbers",
-          style: TextStyle(
-            fontSize: 24,
-            color: Colors.white,
+            ),
           ),
         ),
-      ),
-      body: ListView(
-        children: [
-          NumberItem(number: one),
-        ],
-      )
-    );
+        body: ListView(
+          children: [
+            NumberItem(number: numbers[0]),
+            NumberItem(number: numbers[1]),
+            NumberItem(number: numbers[2]),
+            NumberItem(number: numbers[3]),
+            NumberItem(number: numbers[4]),
+            NumberItem(number: numbers[5]),
+            NumberItem(number: numbers[6]),
+            NumberItem(number: numbers[7]),
+            NumberItem(number: numbers[8]),
+            NumberItem(number: numbers[9]),
+            NumberItem(number: numbers[10]),
+
+          ],
+        ));
   }
 }
-
