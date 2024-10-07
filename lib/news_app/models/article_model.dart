a585b3f769510892d5aac4db4b5d41807a2a13e0
Salah Saleh <salahsaleh1015@gmail.com>
2024-10-07 19:59:45 +0300

    news app ( article model )

---
+++ lib/news_app/models/article_model.dart
@@ -0,0 +1,11 @@
+
+
+
+class ArticleModel {
+
+  final String title;
+  final String? description;
+  final String? urlToImage;
+
+  ArticleModel({required this.title, required this.description, required this.urlToImage});
+}
