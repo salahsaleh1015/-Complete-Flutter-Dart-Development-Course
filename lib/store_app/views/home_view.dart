8e73882f1e923828bac9a752b828f78289d16758
Salah Saleh <salahsaleh1015@gmail.com>
2024-11-12 16:45:11 +0200

    store app (product card UI)

--- lib/store_app/views/home_view.dart
+++ lib/store_app/views/home_view.dart
@@ -1,21 +1,84 @@
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView ({super.key});
- static String id = 'HomeView';
+  const HomeView({super.key});
+  static String id = 'HomeView';
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.cartPlus),),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(FontAwesomeIcons.cartPlus),
+          ),
         ],
         centerTitle: true,
         backgroundColor: Colors.white,
         title: Text("New Trend"),
+      ),
+      body: Center(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: 135,
+              width: 240,
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    blurRadius: 10,
+                    spreadRadius: 2,
+                    color: Colors.grey.withOpacity(0.2))
+              ]),
+              child: const Card(
+                color: Colors.white,
+                elevation: 10,
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "HandBag LV",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            r"$225",
+                            style: TextStyle(color: Colors.black, fontSize: 18),
+                          ),
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                            size: 25,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+                bottom: 90,
+                left: 140,
+                child: Image.network(
+                  "https://m.media-amazon.com/images/I/5184gJbdXLL._AC_SX679_.jpg",
+                  height: 100,
+                ))
+          ],
+        ),
       ),
     );
   }
