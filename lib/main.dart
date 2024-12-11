d09840e166b49df4c6e7f4deff3717b66eec3017
Salah Saleh <salahsaleh1015@gmail.com>
2024-12-11 21:30:30 +0200

    Notes App (init hive)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:complete_flutter_dart_development_course/notes_app/constants.dart';
 import 'package:complete_flutter_dart_development_course/notes_app/views/notes_view.dart';
 import 'package:complete_flutter_dart_development_course/store_app/views/home_view.dart';
 import 'package:complete_flutter_dart_development_course/store_app/views/update_product_view.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async{
+
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
--- lib/notes_app/constants.dart
+++ lib/notes_app/constants.dart
@@ -3,3 +3,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff53ebd6);
+const kNotesBox = "notes_box";
