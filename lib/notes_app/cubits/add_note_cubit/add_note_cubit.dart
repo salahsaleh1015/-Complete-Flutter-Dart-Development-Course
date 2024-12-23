251ae50b3f39dbe0ad35f6d36161c9e9e32cc2ab
Salah Saleh <salahsaleh1015@gmail.com>
2024-12-23 08:19:58 +0200

    Notes App (add note method)

--- lib/notes_app/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/notes_app/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:complete_flutter_dart_development_course/notes_app/constants.dart';
+import 'package:complete_flutter_dart_development_course/notes_app/models/note_model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailure(errorMessage: e.toString()));
+    }
+  }
 }
