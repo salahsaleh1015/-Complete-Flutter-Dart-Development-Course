36734629bb8251442682519d3593b179f4403aa9
Salah Saleh <salahsaleh1015@gmail.com>
2024-12-23 09:14:49 +0200

    Notes App ( add note cubit to bottom sheet)

---
+++ lib/notes_app/widgets/add_bottom_modal_sheet_form.dart
@@ -0,0 +1,75 @@
+
+
+
+
+
+import 'package:complete_flutter_dart_development_course/notes_app/widgets/custom_bottom.dart';
+import 'package:complete_flutter_dart_development_course/notes_app/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+
+class AddModalBottomSheetForm extends StatefulWidget {
+  const AddModalBottomSheetForm({
+    super.key,
+  });
+
+  @override
+  State<AddModalBottomSheetForm> createState() => _AddModalBottomSheetFormState();
+}
+
+class _AddModalBottomSheetFormState extends State<AddModalBottomSheetForm> {
+
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+
+  String? title, content;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      autovalidateMode: autoValidateMode,
+      key: formKey,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          CustomTextField(
+            onSaved: (val){
+              title = val;
+            },
+            hintText: "title",
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          CustomTextField(
+            onSaved: (val){
+              content = val;
+            },
+            hintText: "content",
+            maxLines: 5,
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          CustomButton(
+            onTap: (){
+              if(formKey.currentState!.validate()){
+                formKey.currentState!.save();
+              }else{
+                autoValidateMode = AutovalidateMode.always;
+                setState(() {
+
+                });
+              }
+            },
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
--- lib/notes_app/widgets/bottom_modal_sheet.dart
+++ lib/notes_app/widgets/bottom_modal_sheet.dart
@@ -1,87 +1,38 @@
 import 'package:complete_flutter_dart_development_course/notes_app/constants.dart';
+import 'package:complete_flutter_dart_development_course/notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:complete_flutter_dart_development_course/notes_app/widgets/add_bottom_modal_sheet_form.dart';
 import 'package:complete_flutter_dart_development_course/notes_app/widgets/custom_bottom.dart';
 import 'package:complete_flutter_dart_development_course/notes_app/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddModalBottomSheet extends StatelessWidget {
   const AddModalBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return  SizedBox(
         width: double.infinity,
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: SingleChildScrollView(
-            child: AddModalBottomSheetForm(),
+            child: BlocConsumer<AddNoteCubit, AddNoteState>(
+              listener: (context, state) {
+              if(state is AddNoteFailure){
+                debugPrint(" error message  ${state.errorMessage}");
+              }
+
+              if(state is AddNoteSuccess){}
+              Navigator.pop(context);
+              },
+              builder: (context, state) {
+                return ModalProgressHUD(
+                    inAsyncCall: state is AddNoteLoading ? true : false,
+                    child: AddModalBottomSheetForm());
+              },
+            ),
           ),
         ));
   }
 }
-
-class AddModalBottomSheetForm extends StatefulWidget {
-  const AddModalBottomSheetForm({
-    super.key,
-  });
-
-  @override
-  State<AddModalBottomSheetForm> createState() => _AddModalBottomSheetFormState();
-}
-
-class _AddModalBottomSheetFormState extends State<AddModalBottomSheetForm> {
-
-  final GlobalKey<FormState> formKey = GlobalKey();
-
-  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
-
-  String? title, content;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      autovalidateMode: autoValidateMode,
-      key: formKey,
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          CustomTextField(
-             onSaved: (val){
-               title = val;
-             },
-            hintText: "title",
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          CustomTextField(
-            onSaved: (val){
-              content = val;
-            },
-            hintText: "content",
-            maxLines: 5,
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          CustomButton(
-            onTap: (){
-              if(formKey.currentState!.validate()){
-                formKey.currentState!.save();
-              }else{
-                autoValidateMode = AutovalidateMode.always;
-                setState(() {
-
-                });
-              }
-            },
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
