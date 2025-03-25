27a0702eb6425fd7c114dcb126ac9439ee60e082
Salah Saleh <salahsaleh1015@gmail.com>
2025-03-25 23:00:30 +0200

    Scholar Chat App (add future builder)

--- lib/scholar_chat_app/views/chat_view.dart
+++ lib/scholar_chat_app/views/chat_view.dart
@@ -1,69 +1,67 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:complete_flutter_dart_development_course/scholar_chat_app/constants.dart';
+import 'package:complete_flutter_dart_development_course/scholar_chat_app/widgets/chat_bubble.dart';
 import 'package:flutter/material.dart';
 class ChatView extends StatelessWidget {
-  const ChatView({super.key});
-
+   ChatView({super.key});
 
   static String id = 'ChatView';
+
+  CollectionReference message = FirebaseFirestore.instance.collection('messages');
+  TextEditingController messageController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-
-      appBar:  AppBar(
-        backgroundColor: kPrimaryColor,
-        title: const Text("Scholar Chat",style: TextStyle(color: Colors.white),),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-
-              itemBuilder: (context, index)=> const Align(
-                alignment: Alignment.topLeft,
-                child: ChatBubble(),
-              ),
+    return  FutureBuilder<QuerySnapshot>(
+      future: message.get(),
+      builder:(context,snapshot){
+        if(snapshot.hasData){
+          print(snapshot.data!.docs[0]['message']);
+          return Scaffold(
+            appBar:  AppBar(
+              backgroundColor: kPrimaryColor,
+              title: const Text("Scholar Chat",style: TextStyle(color: Colors.white),),
+              centerTitle: true,
+              automaticallyImplyLeading: false,
             ),
-          ),
-          Padding(padding: const EdgeInsets.all(32.0),
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
 
-          child: TextField(
-            decoration: InputDecoration(
-              suffixIcon: const Icon(Icons.send,color: kPrimaryColor,),
-              border: OutlineInputBorder(
+                    itemBuilder: (context, index)=> const Align(
+                      alignment: Alignment.topLeft,
+                      child: ChatBubble(),
+                    ),
+                  ),
+                ),
+                Padding(padding: const EdgeInsets.all(32.0),
 
-                borderRadius: BorderRadius.circular(16),
-                borderSide: const BorderSide(width: 0,color: kPrimaryColor),
-              )
+                  child: TextField(
+                    controller: messageController,
+                    onSubmitted: (data){
+                      message.add({
+                        'message':data
+                      });
+                      messageController.clear();
+                    },
+                    decoration: InputDecoration(
+                        suffixIcon: const Icon(Icons.send,color: kPrimaryColor,),
+                        border: OutlineInputBorder(
+
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: const BorderSide(width: 0,color: kPrimaryColor),
+                        )
+                    ),
+                  ),
+                )
+              ],
             ),
-          ),
-          )
-        ],
-      ),
+          );
+        }else{
+          return const Scaffold(body: Center(child: Text("Loading..."),));
+        }
+      }
     );
   }
 }
 
-class ChatBubble extends StatelessWidget {
-  const ChatBubble({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(16),
-      decoration: const BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
-          bottomRight: Radius.circular(30),
-        )
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
-      child: const Text("Hello World ",style: TextStyle(color: Colors.white),),);
-  }
-}
